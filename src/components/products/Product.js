6b7851fcc233887b6d0d6361e01a044dbefc2b5f
Sarath T S <sarathiscookie@hotmail.com>
2021-06-18 23:41:11 +0530

    Adding keys to the product list.

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import ProductStore from "./ProductStore";
 
@@ -7,13 +7,14 @@ import ProductList from "./ProductList";
 function Product(props) {
   const products = [
     {
+      id: 1,
       name: "Iphone 12",
       quantity: 5,
       price: 95000,
       tax: 1000,
       description: "Mobile",
     },
-    {
+    { id: 2,
       name: "Samsung A3",
       quantity: 4,
       price: 55000,
@@ -21,6 +22,7 @@ function Product(props) {
       description: "Mobile",
     },
     {
+      id: 3,
       name: "Iphone 11",
       quantity: 2,
       price: 65000,
@@ -32,7 +34,7 @@ function Product(props) {
   return (
     <div>
       <ProductStore />
-      {products.map(product => <ProductList name={product.name} quantity={product.quantity} price={product.price} tax={product.tax} description={product.description} />)}
+      {products.map(product => <ProductList key={product.id} name={product.name} quantity={product.quantity} price={product.price} tax={product.tax} description={product.description} />)}
     </div>
   );
 }
