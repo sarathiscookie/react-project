2145f93f6280f3a5f39c5448c6b024f4f6271aab
Sarath T S <sarathiscookie@hotmail.com>
2021-07-27 23:43:19 +0530

    Topics: Fragment - Short syntax to declare fragment "<> </>".

---
+++ src/components/topics/ColumnsTwo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ColumnsTwo extends Component {
+  render() {
+    return (
+      <>
+        <td>Welcome</td>
+        <td>Onboard</td>
+      </>
+    );
+  }
+}
